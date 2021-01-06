ea3846fb028180a35d0410f265c37a6ada823745
Pai Yen Chang <ken1100327@gmail.com>
2021-01-06 23:34:56 +0800

    顯示所有公告 & Controller

--- resources/views/doctors/announcements/show.blade.php
+++ resources/views/doctors/announcements/show.blade.php
@@ -1,1 +1,33 @@
-<?php
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th width="30" style="text-align: center">#</th>
+                        <th>標題</th>
+                        <th width="70" style="text-align: center">內容</th>
+                        <th width="100" style="text-align: center">功能</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach($announcements as $announcement)
+                            <tr>
+                                <td style="text-align: center">{{ $announcement->id }}</td>
+                                <td>{{ $announcement->title }}</td>
+                                <td>{{ $announcement->content }}</td>
+                                <td>
+                                    <a href="{{ route('doctors.announcements.edit', $announcement->id) }}">編輯</a>
+                                    /
+                                    <a href="#">刪除</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
