8ee52820dd8dbda409320478d61c04efbb3e187a
Pai Yen Chang <ken1100327@gmail.com>
2021-01-07 21:48:33 +0800

    修改 公告的 edit.blade.php (加入編輯的表單)

--- resources/views/doctors/announcements/edit.blade.php
+++ resources/views/doctors/announcements/edit.blade.php
@@ -1,1 +1,26 @@
-<?php
+<div class="row">
+    <div class="col-lg-12">
+
+            <form action="{{ route('doctors.announcements.update', $announcements->id) }}" method="POST" role="form">
+                @method('PATCH')
+                @csrf
+
+                <div class="form-group">
+
+                    <label for="title">標題：</label>
+                    <input name="title" class="form-control" placeholder="請輸入文章標題" value="{{ old('title', $announcements->title) }}">
+                </div>
+
+                <div class="form-group">
+
+                    <label for="content">內容：</label>
+                    <textarea id="content" name="content" class="form-control" rows="10">{{ old('content', $announcements->content) }}</textarea>
+                </div>
+
+                <div class="text-right">
+                    <button type="submit" class="btn btn-success">更新</button>
+                </div>
+
+            </form>
+    </div>
+</div>
