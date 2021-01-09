bab3d20e4fa074e8af81e09cdcdb12c27254a5fc
林駿綦 <r10101720307@gmail.com>
2021-01-10 00:51:18 +0800

    修改 view（顯示門診表）

--- resources/views/doctors/outpatients/show.blade.php
+++ resources/views/doctors/outpatients/show.blade.php
@@ -7,15 +7,20 @@
 <body>
 
 
-
 <?php
+use Carbon\Carbon;
+
 $a = "上午";
 $b = "下午";
 $c = "晚上";
 $count = 0;
+$date = array();
+
+for($i=0; $i<7; $i++){
+    $date[$i] = Carbon::createFromDate(2021, 01, 04, 'Asia/Taipei')->addDay($i)->toDateString();
+}
+
 ?>
-
-
 
 
 <form action="" align="center">
@@ -23,73 +28,99 @@ $count = 0;
 
     <table border="1" align="center" height="500" width="800">
         <tr>
+
             <td>時段</td>
-            <td>星期一</td>
-            <td>星期二</td>
-            <td>星期三</td>
-            <td>星期四</td>
-            <td>星期五</td>
-            <td>星期六</td>
-            <td>星期日</td>
+            <td>上午</td>
+            <td>下午</td>
+            <td>晚上</td>
+
         </tr>
         <tr>
-            <td>上午</td>
+            <td>{{$date[0]}}(星期一)</td>
+
             @for($i=0; $i<21; $i++)
-                @if($i<7)
 
-                    <td>{{ $tests[$i]->name }}</td>
+                @if($i<3)
 
-                    @if($i==6)
-                        </tr><tr>
-                            <td>下午</td>
-                    @endif
+                    <td>
+                        {{ $tests[$i]->name }}
+                    </td>
+
+                    @if($i==2)
+        </tr><tr>
+            <td>{{$date[1]}}(星期二)</td>
+            @endif
 
 
-            @elseif($i>=7 and $i<14)
+            @elseif($i>=3 and $i<6)
 
-                <td>{{ $tests[$i]->name }}</td>
-                    @if($i==13)
-                        </tr><tr>
-                            <td>晚上</td>
-                    @endif
+                <td>
+                    {{ $tests[$i]->name }}
+                </td>
+
+                @if($i==5)
+        </tr><tr>
+            <td>{{$date[2]}}(星期三)</td>
+            @endif
+            @elseif($i>=6 and $i<9)
+
+                <td>
+                    {{ $tests[$i]->name }}
+                </td>
+
+                @if($i==8)
+        </tr><tr>
+            <td>{{$date[3]}}(星期四)</td>
+            @endif
+            @elseif($i>=9 and $i<12)
+
+                <td>
+                    {{ $tests[$i]->name }}
+                </td>
+
+                @if($i==11)
+        </tr><tr>
+            <td>{{$date[4]}}(星期五)</td>
+            @endif
+            @elseif($i>=12 and $i<15)
+
+                <td>
+                    {{ $tests[$i]->name }}
+                </td>
+
+                @if($i==14)
+        </tr><tr>
+            <td>{{$date[5]}}(星期六)</td>
+            @endif
+            @elseif($i>=15 and $i<18)
+
+                <td>
+                    {{ $tests[$i]->name }}
+                </td>
+
+                @if($i==17)
+        </tr><tr>
+            <td>{{$date[6]}}(星期日)</td>
+            @endif
             @else
 
-                <td>{{ $tests[$i]->name }}</td>
+                <td>
+                    {{ $tests[$i]->name }}
+                </td>
             @endif
 
             @endfor</tr></table>
 
+
+
+
+
+
+
+
+
+
 </form>
-
-
-
-
-
-{{--<table class="table table-striped task-table">--}}
-{{--    <thead>--}}
-{{--    <th>任務</th>--}}
-{{--    <th>&nbsp;</th>--}}
-{{--    </thead>--}}
-{{--    <tbody>--}}
-{{--    @foreach ($a as $b)--}}
-{{--        <tr>--}}
-{{--            <td class="table-text"><div>{{ $b->date }}</div></td>--}}
-
-{{--            <!-- 任務刪除按鈕 -->--}}
-{{--            <td class="table-text"><div>{{ $b->period }}</div></td>--}}
-{{--        </tr>--}}
-{{--    @endforeach--}}
-{{--    </tbody>--}}
-{{--</table>--}}
-
-{{--@foreach ($aa as $aaa)--}}
-{{--    {{ $aaa->name }}--}}
-{{--@endforeach--}}
-
-{{--@foreach ($bb as $bbb)--}}
-{{--    {{ $bbb->date }}--}}
-{{--@endforeach--}}
-
 
 
 </body>
