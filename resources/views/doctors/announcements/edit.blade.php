3a9a76da87dbebf14dbf8273beffd1be63eae7c6
Pai Yen Chang <ken1100327@gmail.com>
2021-01-12 01:24:32 +0800

    修改 view（修改公告）

--- resources/views/doctors/announcements/edit.blade.php
+++ resources/views/doctors/announcements/edit.blade.php
@@ -1,26 +1,89 @@
-<div class="row">
-    <div class="col-lg-12">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('修改公告') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             <form action="{{ route('doctors.announcements.update', $announcements->id) }}" method="POST" role="form">
-                @method('PATCH')
-                @csrf
+            @method('PATCH')
+            @csrf
 
-                <div class="form-group">
+            <!-- This example requires Tailwind CSS v2.0+ -->
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 
-                    <label for="title">標題：</label>
-                    <input name="title" class="form-control" placeholder="請輸入文章標題" value="{{ old('title', $announcements->title) }}">
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        標題：
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                <input name="title" class="form-control" placeholder="請輸入文章標題"
+                                                       value="{{ old('title', $announcements->title) }}">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        內容：
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                <textarea id="content" name="content" class="form-control"
+                                                          rows="10">{{ old('content', $announcements->content) }}</textarea>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <!-- More rows... -->
+                                    </tbody>
+                                </table>
+
+                                <div class="flex items-center justify-end mt-4 px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <button onclick="history.back()"
+                                            class="text-indigo-600 hover:text-indigo-900 text-2xl">返回
+                                    </button>
+                                    &emsp; | &emsp;
+                                    <button type="submit" class="text-indigo-600 hover:text-indigo-900 text-2xl">更新
+                                    </button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="form-group">
-
-                    <label for="content">內容：</label>
-                    <textarea id="content" name="content" class="form-control" rows="10">{{ old('content', $announcements->content) }}</textarea>
-                </div>
-
-                <div class="text-right">
-                    <button type="submit" class="btn btn-success">更新</button>
-                </div>
-
             </form>
+        </div>
     </div>
-</div>
+</x-app-layout>
