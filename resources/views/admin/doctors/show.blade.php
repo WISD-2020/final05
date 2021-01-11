43ad9d5401c87e63b53b31476e5f05e7fe1626c7
林駿綦 <r10101720307@gmail.com>
2021-01-12 01:27:40 +0800

    修改 view（後臺顯示醫師）

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -1,22 +1,101 @@
-<tr>
-    <td><a class="btn btn-sm btn-primary" href="{{ route('admin.doctors.create') }}">新增</a></td>
-</tr>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('新增醫師') }}
+        </h2>
+    </x-slot>
 
-@foreach ($doctors as $doctor)
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        名字
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        性別
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        生日
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        電話
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        email
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        狀態
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 
-    <tr>
+                                    </th>
+                                </tr>
+                                </thead>
+                                @foreach ($doctors as $doctor)
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ $doctor->name }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{{ $doctor->sex }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{{ $doctor->birthday }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{{ $doctor->telephone }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{{ $doctor->email }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        @if($doctor->status =="1")
+                                                <div class="text-sm text-gray-900 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">{{ __('在職') }}</div>
+                                        @elseif($doctor->status =="2")
+                                                <div class="text-sm text-gray-900 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-green-800">{{ __('停職') }}</div>
+                                        @endif
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="{{ route("admin.doctors.edit", $doctor->id) }}" class="text-indigo-600 hover:text-indigo-900 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-green-800">編輯</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                    <!-- More rows... -->
+                                    </tbody>
+                            </table>
+                            <div
+                                class="flex items-center justify-end mt-4 px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="{{ route("admin.doctors.create") }}"
+                                   class="text-indigo-600 hover:text-indigo-900 text-2xl">新增</a>
 
-        <td class="table-text"><div>{{ $doctor->name }}</div></td>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-        <td>{{ $doctor->sex }}</td>
-        <td>{{ $doctor->birthday }}</td>
-        <td>{{ $doctor->status }}</td>
-        <td>{{ $doctor->account }}</td>
-        <td>{{ $doctor->telephone }}</td>
-
-
-        <td><a class="btn btn-sm btn-primary" href="{{ route('admin.doctors.edit', $doctor->id) }}">編輯</a></td>
-
-    </tr>
-@endforeach
+        </div>
+    </div>
+</x-app-layout>
