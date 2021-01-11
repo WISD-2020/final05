d84883401831e9c255f39e2d84b559b4e16325d6
林駿綦 <r10101720307@gmail.com>
2021-01-11 22:55:18 +0800

    修改 view（新增公告）

--- resources/views/doctors/announcements/create.blade.php
+++ resources/views/doctors/announcements/create.blade.php
@@ -1,21 +1,88 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('新增公告') }}
+        </h2>
+    </x-slot>
 
-    <form method="POST" action="{{ route('doctors.announcements.store') }}">
-        @method('POST')
-        @csrf
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <div>
-                <x-jet-label for="title" value="{{ __('標題') }}" />
-                <x-jet-input id="title" class="block mt-1 w-full" type="text" name="title" required />
-            </div>
+            <form method="POST" action="{{ route('doctors.announcements.store') }}">
+            @method('POST')
+            @csrf
 
-            <div class="mt-4">
-                <x-jet-label for="content" value="{{ __('內容') }}" />
-                <x-jet-input id="content" class="block mt-1 w-full" type="textarea" name="content" maxlength="500" required />
-            </div>
+            <!-- This example requires Tailwind CSS v2.0+ -->
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button class="ml-4">
-                    {{ __('新增') }}
-                </x-jet-button>
-            </div>
-        </form>
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        標題
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                <x-jet-input id="title" class="block mt-1 w-full" type="text" name="title" required/>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        內容
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">
+                                                <x-jet-input id="content" class="block mt-1 w-full" type="textarea" name="content" maxlength="500" required/>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <!-- More rows... -->
+                                    </tbody>
+                                </table>
+
+                                <div
+                                    class="flex items-center justify-end mt-4 px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <button onclick="history.back()"
+                                            class="text-indigo-600 hover:text-indigo-900 text-2xl">返回
+                                    </button>
+                                    &emsp; | &emsp;
+                                    <button type="submit" class="text-indigo-600 hover:text-indigo-900 text-2xl">送出
+                                    </button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-app-layout>
