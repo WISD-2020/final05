67d58be5cc046ebaf6847517124724a3a1d7ed09
Pai Yen Chang <ken1100327@gmail.com>
2021-01-13 03:03:25 +0800

    修改 view（查詢掛號頁面）

--- resources/views/patients/appointments/records/show.blade.php
+++ resources/views/patients/appointments/records/show.blade.php
@@ -1,1 +1,99 @@
-<?php
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('查詢掛號') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        名字
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        掛號日期
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        掛號時段
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        醫師名字
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        功能
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach($appointments as $appointment)
+                                    <tr>
+                                        @foreach($users as $user)
+                                            @if($appointment->user_id == $user->id)
+                                                <td>{{ $user->name }}</td>
+                                            @endif
+                                        @endforeach
+
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ $appointment->date }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        {{ $appointment->period }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+
+
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="ml-4">
+                                                    <div class="text-sm font-medium text-gray-900">
+                                                        @for($i=0;$i<count($doctor_name);$i++)
+                                                            @if($appointment->outpatient_id == $doctor_name[$i]->id)
+                                                            {{ $doctor_name[$i]->name }}
+                                                            @endif
+                                                        @endfor
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            <a href="{{ route('patients.appointments.edit', $appointment->id) }}"><button class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800" type="submit">編輯</button></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+
+
+                                <!-- More rows... -->
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
