4a22590d3cb8b9a45eee88a1927e564e6899ef0f
Pai Yen Chang <ken1100327@gmail.com>
2021-01-13 08:52:42 +0800

    修改 Controller（看診）

--- app/Http/Controllers/TreatmentController.php
+++ app/Http/Controllers/TreatmentController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
+use App\Models\Outpatient;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TreatmentController extends Controller
 {
@@ -13,7 +17,24 @@ class TreatmentController extends Controller
      */
     public function index()
     {
-        //
+        $auth_id=auth()->user()->id;
+        $users = User::Get();
+        //登入者的掛號資料
+        $outpatients = Outpatient::orderBy('date', 'DESC')->get();
+        //有掛號的病人姓名
+        $patient_name = DB::table('appointments')
+            ->join('users', 'appointments.user_id', '=', 'users.id')
+            ->select('users.name','appointments.user_id')
+            ->get();
+dd($patient_name[]->user_id);
+        $data = [
+            'outpatients'=>$outpatients,
+            'users' => $users,
+            'auth_id' => $auth_id,
+            'patient_name' => $patient_name,
+        ];
+
+        return view('doctors.treatments.show', $data);
     }
 
     /**
@@ -54,9 +75,14 @@ class TreatmentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($user_id)
     {
-        //
+        dd($user_id);
+
+        $data = [
+            'user_id' => $user_id,
+        ];
+        return view('doctors.treatments.edit', $data);
     }
 
     /**
@@ -68,7 +94,9 @@ class TreatmentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $doctor->update($request->all());
+
+        return redirect()->route('admin.doctors.index');
     }
 
     /**
