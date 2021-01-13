4caf0fd84d325afa51f5beee7ab0b680a0c75cfc
Pai Yen Chang <ken1100327@gmail.com>
2021-01-13 08:56:27 +0800

    修改文字

--- resources/views/admin/doctors/edit.blade.php
+++ resources/views/admin/doctors/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('新增公告') }}
+            {{ __('醫師狀態') }}
         </h2>
     </x-slot>
 
