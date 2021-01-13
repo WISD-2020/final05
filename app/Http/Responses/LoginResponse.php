3b7761d92eab3e42aac694f50e02d73cf9779692
Pai Yen Chang <ken1100327@gmail.com>
2021-01-13 08:55:18 +0800

    修改登入跳轉頁面

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -16,15 +16,17 @@ class LoginResponse implements LoginResponseContract
         // the user can be located with Auth facade
 
         $status=Auth::user()->status;
+        $id=Auth::user()->id;
+
 
         if ($status=="2")
             return "<script language='JavaScript'>alert('此帳號已停用！！');location.href='/';</script>";
         elseif ($status=="1")
-            return redirect()->route('doctors.edit');
+            return redirect()->route('doctors.treatments.index');
         elseif ($status=="3")
             return redirect()->route('admin.doctors.index');
         elseif ($status=="0")
-            return redirect()->route('dashboard');
+            return redirect()->route('patients.appointments.create');
 
 
     }
